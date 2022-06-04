53cebc315334a81cab05318ef9fa80d43283b410
par-ft <parisafakhr79@gmail.com>
2022-06-04 15:06:44 +0430

    minor improvements

--- include/q1.h
+++ include/q1.h
@@ -7,7 +7,7 @@
 namespace q1
 {
     template <typename T, typename Func = std::function<T(T)>>
-    inline T gradient_descent(T initial_value, T step, Func func = Func{})
+    T gradient_descent(T initial_value, T step, Func func = Func{})
     {
         T h{static_cast<T>(pow(10, -7))};
         T optimum{initial_value};
--- include/q2.h
+++ include/q2.h
@@ -3,7 +3,9 @@
 
 #include <string>
 #include <vector>
-
+#include <iostream>
+#include <fstream>
+#include <sstream>
 namespace q2
 {
     struct Patient
@@ -15,11 +17,48 @@ namespace q2
         size_t alkhol;
     };
 
-    std::vector<Patient> read_file(std::string filename)
+    inline std::vector<Patient> read_file(std::string filename)
     {
+        std::vector<Patient> patients;
+        std::vector<std::string> row;
+        std::string line, word;
+
+        std::fstream file(filename, std::ios::in);
+        if (file.is_open())
+        {
+            size_t row_num{};
+            while (getline(file, line))
+            {
+                row.clear();
+
+                std::stringstream str(line);
+                while (getline(str, word, ','))
+                    row.push_back(word);
+
+                if (row_num == 0 || row_num == 1)
+                {
+                    row_num++;
+                    continue;
+                }
+
+                row[0].erase(remove_if(row[0].begin(), row[0].end(), isspace), row[0].end());
+                row[1].erase(remove_if(row[1].begin(), row[1].end(), isspace), row[1].end());
+
+                Patient patient{};
+                patient.name = row[0] + " " + row[1];
+                std::stringstream(row[2]) >> patient.age;
+                std::stringstream(row[3]) >> patient.smokes;
+                std::stringstream(row[4]) >> patient.area_q;
+                std::stringstream(row[5]) >> patient.alkhol;
+
+                patients.push_back(patient);
+            }
+        }
+
+        return patients;
     }
 
-    std::vector<Patient> sort(std::vector<Patient> patients)
+    inline std::vector<Patient> sort(std::vector<Patient> patients)
     {
     }
 }
