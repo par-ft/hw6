50824ddd5e3f0fe419ba870d00749758d7c8dbfc
par-ft <parisafakhr79@gmail.com>
2022-06-04 17:43:48 +0430

    first steps to solve problem 3

--- include/q3.h
+++ include/q3.h
@@ -1,4 +1,23 @@
 #ifndef Q3_H
 #define Q3_H
 
-#endif //Q3_H
+#include <queue>
+
+namespace q3
+{
+    struct Flight
+    {
+        std::string flight_number;
+        size_t duration;
+        size_t connections;
+        size_t connection_times;
+        size_t price;
+    };
+
+    std::priority_queue<Flight> gather_flights(std::string filename)
+    {
+    }
+
+}
+
+#endif // Q3_H
